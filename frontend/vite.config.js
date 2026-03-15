078536b448094af8906be079bd48335655c897a6
AniketPanchabhai <67269865+AniketPanchabhai@users.noreply.github.com>
2026-03-15 16:23:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: "https://flight-booking-system-2-0qtd.onrender.com",
+    port: "https://flight-booking-system-1-gbwt.onrender.com",
     proxy: {
       '/api': {
         target: 'https://flight-booking-system-1-gbwt.onrender.com',
